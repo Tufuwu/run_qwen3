b6d189ef2156c4852042c3a3faa22e65b10972a3
wu <wu@.com>
2025-08-30 13:07:02 +0800

    run pysmo/pysmo/gpt-4o-1

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,1 @@
-# the inclusion of the tests module is not meant to offer best practices for
-# testing in general, but rather to support the `find_packages` example in
-# setup.py that excludes installing the "tests" package
+from __future__ import absolute_import, division, print_function
