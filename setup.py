179979d622bfd7a8a4db13aac377eb744fe811d3
wu <wu@.com>
2025-08-30 12:52:15 +0800

    run matplotlib/cycler/gpt-4o-1

--- setup.py
+++ setup.py
@@ -1,52 +1,23 @@
 from setuptools import setup
-import codecs
-import os
 
-
-VERSION = '1.4.1'
-AUTHOR_NAME = 'Andy Port'
-AUTHOR_EMAIL = 'AndyAPort@gmail.com'
-
-
-def read(*parts):
-    """
-    Build an absolute path from *parts* and and return the contents of the
-    resulting file.  Assume UTF-8 encoding.
-    """
-    HERE = os.path.abspath(os.path.dirname(__file__))
-    with codecs.open(os.path.join(HERE, *parts), "rb", "utf-8") as f:
-        return f.read()
-
-
-setup(name='svgpathtools',
-      packages=['svgpathtools'],
-      version=VERSION,
-      description=('A collection of tools for manipulating and analyzing SVG '
-                   'Path objects and Bezier curves.'),
-      long_description=read("README.md"),
-      long_description_content_type='text/markdown',
-      author=AUTHOR_NAME,
-      author_email=AUTHOR_EMAIL,
-      url='https://github.com/mathandy/svgpathtools',
-      # download_url = 'http://github.com/mathandy/svgpathtools/tarball/'+VERSION,
-      license='MIT',
-      install_requires=['numpy', 'svgwrite'],
-      platforms="OS Independent",
-      requires=['numpy', 'svgwrite'],
-      keywords=['svg', 'svg path', 'svg.path', 'bezier', 'parse svg path', 'display svg'],
-      classifiers=[
-            "Development Status :: 4 - Beta",
-            "Intended Audience :: Developers",
-            "License :: OSI Approved :: MIT License",
-            "Operating System :: OS Independent",
-            "Programming Language :: Python :: 2",
-            "Programming Language :: Python :: 3",
-            "Topic :: Multimedia :: Graphics :: Editors :: Vector-Based",
-            "Topic :: Scientific/Engineering",
-            "Topic :: Scientific/Engineering :: Image Recognition",
-            "Topic :: Scientific/Engineering :: Information Analysis",
-            "Topic :: Scientific/Engineering :: Mathematics",
-            "Topic :: Scientific/Engineering :: Visualization",
-            "Topic :: Software Development :: Libraries :: Python Modules",
-            ],
+setup(name='cycler',
+      version='0.10.0',
+      author='Thomas A Caswell',
+      author_email='matplotlib-users@python.org',
+      py_modules=['cycler'],
+      description='Composable style cycles',
+      url='https://github.com/matplotlib/cycler',
+      platforms='Cross platform (Linux, Mac OSX, Windows)',
+      license="BSD",
+      python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+      classifiers=['License :: OSI Approved :: BSD License',
+                   'Development Status :: 4 - Beta',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3.6',
+                   ],
+      keywords='cycle kwargs',
       )
