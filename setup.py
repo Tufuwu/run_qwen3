53bbfdebcea5ca6fdc207739f2b46fd63a2c7525
wu <wu@.com>
2025-08-30 13:11:20 +0800

    run revsys/django-tos/gpt-4o-1

--- setup.py
+++ setup.py
@@ -1,47 +1,29 @@
+import os
 from setuptools import setup, find_packages
 
-version = "3.2.dev0"
+from tos import VERSION
 
-with open("README.rst", encoding="UTF-8") as file:
-    long_description = file.read() + "\n"
-
-with open("HISTORY.txt", encoding="UTF-8") as file:
-    long_description += file.read()
+f = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
+readme = f.read()
+f.close()
 
 setup(
-    name="pyroma",
-    version=version,
-    description="Test your project's packaging friendliness",
-    long_description=long_description,
-    python_requires=">=3.6",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: Implementation :: PyPy",
-        "License :: OSI Approved :: MIT License",
-        "Development Status :: 5 - Production/Stable",
-    ],
-    keywords=["pypi", "quality", "testing"],
-    author="Lennart Regebro",
-    author_email="regebro@gmail.com",
-    url="https://github.com/regebro/pyroma",
-    project_urls={"Source Code": "https://github.com/regebro/pyroma"},
-    license="MIT",
-    packages=find_packages(exclude=["ez_setup"]),
+    name='django-tos',
+    version=".".join(map(str, VERSION)),
+    description='django-tos is a reusable Django application for setting Terms of Service.',
+    long_description=readme,
+    author='Frank Wiles',
+    author_email='frank@revsys.com',
+    url='http://github.com/revsys/django-tos/tree/master',
+    packages=find_packages(),
     include_package_data=True,
-    zip_safe=False,
-    install_requires=[
-        "setuptools",
-        "docutils",
-        "pygments",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
     ],
-    entry_points={
-        "console_scripts": ["pyroma = pyroma:main"],
-        "zest.releaser.prereleaser.before": ["pyroma = pyroma:zester"],
-    },
-    test_suite="pyroma",
 )
