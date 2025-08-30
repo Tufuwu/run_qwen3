8d8dd3e8ce2ee178261e3fb7248105e68301dcb4
wu <wu@.com>
2025-08-30 12:17:16 +0800

    run asmeurer/removestar/gpt-4o-1

--- setup.py
+++ setup.py
@@ -1,91 +1,30 @@
-from setuptools import setup, find_packages
-from codecs import open
-from os import path, environ
-import sys
+import setuptools
 
-here = path.abspath(path.dirname(__file__))
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
 
-# Get the long description from the relevant file
-with open(path.join(here, 'docs/description.rst'), encoding='utf-8') as f:
-        long_description = f.read()
+import versioneer
 
-with open(path.join(here, 'VERSION'), mode='r', encoding='utf-8') as version_file:
-        version = version_file.read().strip()
-
-setup(
-    name='pyeapi',
-
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
-    version=version,
-
-    description='Python Client for eAPI',
+setuptools.setup(
+    name="removestar",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author="Aaron Meurer",
+    author_email="asmeurer@gmail.com",
+    description="A tool to automatically replace 'import *' imports with explicit imports in files",
     long_description=long_description,
-
-    # The project's main homepage.
-    url='https://github.com/arista-eosplus/pyeapi',
-
-    # Author details
-    author='Arista EOS+ CS',
-    author_email='eosplus-dev@arista.com',
-
-    # Choose your license
-    license='BSD-3',
-
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    long_description_content_type="text/markdown",
+    url="https://www.asmeurer.com/removestar/",
+    packages=setuptools.find_packages(),
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 4 - Beta',
-
-        'Intended Audience :: System Administrators',
-        'Topic :: System :: Networking',
-
-        'License :: OSI Approved :: BSD License',
-
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-
-    keywords='networking arista eos eapi',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-
-    # List run-time dependencies here.  These will be installed by pip when your
-    # project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['netaddr'],
-
-    # List additional dependencies here (e.g. development dependencies).
-    # You can install these using the following syntax, for example:
-    # $ pip install -e .[dev,test]
-    extras_require={
-        'dev': ['check-manifest', 'pep8', 'pyflakes', 'twine'],
-        'test': ['coverage', 'mock'],
-    },
+    entry_points={'console_scripts': [ 'removestar = removestar.__main__:main']},
+    python_requires= '>=3.6',
+    install_requires=[
+        'pyflakes'
+    ],
+    license='MIT',
 )
-
-def install():
-    if "install" in sys.argv:
-        return True
-    else:
-        return False
-
-# Use the following to dynamically build pyeapi module documentation
-if install() and environ.get('READTHEDOCS'):
-    print('This method is only called by READTHEDOCS.')
-    from subprocess import Popen
-    proc = Popen(['make', 'modules'], cwd='docs/')
-    (_, err) = proc.communicate()
-    return_code = proc.wait()
-
-    if return_code or err:
-        raise ('Failed to make modules.(%s:%s)' % (return_code, err))
