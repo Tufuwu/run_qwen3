1a6e9bfce6b108615a893f5f95d81b1301009d8a
wu <wu@.com>
2025-08-30 13:04:38 +0800

    run pydot/pydot/gpt-4o-1

--- setup.py
+++ setup.py
@@ -1,67 +1,39 @@
 #!/usr/bin/env python
-# Copyright 2005-2011 Divmod, Inc.
-# Copyright 2013 Florent Xicluna.  See LICENSE file for details
-from __future__ import with_statement
-
-import os.path
-
+"""Installation script."""
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
-    extra = {'scripts': ["bin/pyflakes"]}
-else:
-    extra = {
-        'test_suite': 'pyflakes.test',
-        'entry_points': {
-            'console_scripts': ['pyflakes = pyflakes.api:main'],
-        },
-    }
+
+import ast
+import codecs
+import os
+import re
 
 
-def get_version(fname=os.path.join('pyflakes', '__init__.py')):
-    with open(fname) as f:
-        for line in f:
-            if line.startswith('__version__'):
-                return eval(line.split('=')[-1])
+CURRENT_DIR = os.path.dirname(__file__)
 
 
 def get_long_description():
-    descr = []
-    for fname in ('README.rst',):
-        with open(fname) as f:
-            descr.append(f.read())
-    return '\n\n'.join(descr)
+    readme_path = os.path.join(CURRENT_DIR, "README.md")
+    with codecs.open(readme_path, encoding="utf8") as ld_file:
+        return ld_file.read()
+
+
+def get_version():
+    pydot_py = os.path.join(CURRENT_DIR, "src", "pydot", "__init__.py")
+    _version_re = re.compile(r"__version__\s+=\s+(?P<version>.*)")
+    with codecs.open(pydot_py, "r", encoding="utf8") as f:
+        match = _version_re.search(f.read())
+        version = match.group("version") if match is not None else '"unknown"'
+    return str(ast.literal_eval(version))
 
 
 setup(
-    name="pyflakes",
-    license="MIT",
+    name="pydot",
     version=get_version(),
-    description="passive checker of Python programs",
+    package_dir={"": "src"},
+    packages=["pydot"],
     long_description=get_long_description(),
-    author="A lot of people",
-    author_email="code-quality@python.org",
-    url="https://github.com/PyCQA/pyflakes",
-    packages=["pyflakes", "pyflakes.scripts", "pyflakes.test"],
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
-    classifiers=[
-        "Development Status :: 6 - Mature",
-        "Environment :: Console",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
-        "Topic :: Software Development",
-        "Topic :: Utilities",
-    ],
-    **extra)
+    long_description_content_type="text/markdown",
+)
