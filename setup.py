9a4d5bcbcb10c8970e0b543dac6de2810ee32d4d
wu <wu@.com>
2025-08-30 12:40:46 +0800

    run gpodder/mygpoclient/gpt-4o-1

--- setup.py
+++ setup.py
@@ -1,147 +1,71 @@
 #!/usr/bin/env python
-# usage: use the Makefile instead of invoking this script directly.
-# pylint: disable=import-error,no-name-in-module
-from __future__ import absolute_import, division, print_function, unicode_literals
-from glob import glob
-from distutils.command import build_scripts
+# Generic setup script for single-package Python projects
+# by Thomas Perl <thp.io/about>
+
 from distutils.core import setup
-import os
+
 import re
-import sys
+import os
+import glob
 
-sys.path.insert(1, os.path.dirname(__file__))
-from extras import cmdclass
+PACKAGE = 'mygpoclient'
+SCRIPT_FILE = os.path.join(PACKAGE, '__init__.py')
 
-# Hack: prevent python2's ascii default encoding from breaking inside
-# distutils when installing from utf-8 paths.
-if sys.version_info[0] < 3:
-    # pylint: disable=reload-builtin,undefined-variable
-    reload(sys)  # noqa
-    # pylint: disable=no-member
-    sys.setdefaultencoding('utf-8')
+main_py = open(SCRIPT_FILE).read()
+metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", main_py))
+docstrings = re.findall('"""(.*?)"""', main_py, re.DOTALL)
 
-# Prevent distutils from changing "#!/usr/bin/env python" when
-# --use-env-python is specified.
-try:
-    sys.argv.remove('--use-env-python')
-    use_env_python = True
-except ValueError:
-    use_env_python = False
-if use_env_python:
-    if sys.version_info[0] < 3:
-        # Python2 accepts the r'' unicode literal.
-        pattern = re.compile(r'^should not match$')
-    else:
-        # Python3 reads files as bytes and requires that the regex pattern is
-        # specified as bytes.
-        pattern = re.compile(b'^should not match$')
-    build_scripts.first_line_re = pattern
+# List the packages that need to be installed/packaged
+PACKAGES = (
+        PACKAGE,
+)
 
-# Disable installation of the private cola package by passing --no-private-libs or
-# by setting GIT_COLA_NO_PRIVATE_LIBS=1 in th environment.
-try:
-    sys.argv.remove('--no-private-libs')
-    private_libs = False
-except ValueError:
-    private_libs = not os.getenv('GIT_COLA_NO_PRIVATE_LIBS', '')
+SCRIPTS = glob.glob('bin/*')
 
-# Disable vendoring of qtpy and friends by passing --no-vendor-libs to setup.py or
-# by setting GIT_COLA_NO_VENDOR_LIBS=1 in the environment.
-try:
-    sys.argv.remove('--no-vendor-libs')
-    vendor_libs = False
-except ValueError:
-    vendor_libs = not os.getenv('GIT_COLA_NO_VENDOR_LIBS', '')
+# Metadata fields extracted from SCRIPT_FILE
+AUTHOR_EMAIL = metadata['author']
+VERSION = metadata['version']
+WEBSITE = metadata['website']
+LICENSE = metadata['license']
+DESCRIPTION = docstrings[0].strip()
+if '\n\n' in DESCRIPTION:
+    DESCRIPTION, LONG_DESCRIPTION = DESCRIPTION.split('\n\n', 1)
+else:
+    LONG_DESCRIPTION = None
 
-# fmt: off
-here = os.path.dirname(__file__)
-version = os.path.join(here, 'cola', '_version.py')
-scope = {}
-# flake8: noqa
-exec(open(version).read(), scope)  # pylint: disable=exec-used
-version = scope['VERSION']
-# fmt: on
+# Extract name and e-mail ("Firstname Lastname <mail@example.org>")
+AUTHOR, EMAIL = re.match(r'(.*) <(.*)>', AUTHOR_EMAIL).groups()
 
+DATA_FILES = [
+    ('share/man/man1', glob.glob('man/*')),
+]
 
-def main():
-    """Runs distutils.setup()"""
-    scripts = [
-        'bin/git-cola',
-        'bin/git-cola-sequence-editor',
-        'bin/git-dag',
-    ]
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+]
 
-    if sys.platform == 'win32':
-        scripts.append('contrib/win32/cola')
-
-    packages = [str('cola'), str('cola.models'), str('cola.widgets')]
-
-    setup(
-        name='git-cola',
-        version=version,
-        description='The highly caffeinated git GUI',
-        long_description='A sleek and powerful git GUI',
-        license='GPLv2',
-        author='David Aguilar',
-        author_email='davvid@gmail.com',
-        url='https://git-cola.github.io/',
-        scripts=scripts,
-        cmdclass=cmdclass,
-        packages=packages,
-        platforms='any',
-        data_files=_data_files(),
+setup(name=PACKAGE,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      author=AUTHOR,
+      author_email=EMAIL,
+      license=LICENSE,
+      url=WEBSITE,
+      packages=PACKAGES,
+      scripts=SCRIPTS,
+      data_files=DATA_FILES,
+      download_url=WEBSITE+PACKAGE+'-'+VERSION+'.tar.gz',
+      classifiers=CLASSIFIERS,
     )
 
-
-def _data_files():
-    """Return the list of data files"""
-    data = [
-        _app_path('share/git-cola/bin', '*'),
-        _app_path('share/git-cola/icons', '*.png'),
-        _app_path('share/git-cola/icons', '*.svg'),
-        _app_path('share/git-cola/icons/dark', '*.png'),
-        _app_path('share/git-cola/icons/dark', '*.svg'),
-        _app_path('share/metainfo', '*.xml'),
-        _app_path('share/applications', '*.desktop'),
-        _app_path('share/doc/git-cola', '*.rst'),
-        _app_path('share/doc/git-cola', '*.html'),
-    ]
-
-    if private_libs:
-        data.extend(
-            [_package('cola'), _package('cola.models'), _package('cola.widgets')]
-        )
-
-    if vendor_libs:
-        data.extend([_package('qtpy'), _package('qtpy._patch')])
-
-    data.extend(
-        [
-            _app_path(localedir, 'git-cola.mo')
-            for localedir in glob('share/locale/*/LC_MESSAGES')
-        ]
-    )
-    return data
-
-
-def _package(package, subdirs=None):
-    """Collect python files for a given python "package" name"""
-    dirs = package.split('.')
-    app_dir = _lib_path(*dirs)
-    if subdirs:
-        dirs = list(subdirs) + dirs
-    src_dir = os.path.join(*dirs)
-    return (app_dir, glob(os.path.join(src_dir, '*.py')))
-
-
-def _lib_path(*dirs):
-    return os.path.join('share', 'git-cola', 'lib', *dirs)
-
-
-def _app_path(dirname, entry):
-    """Construct (dirname, [glob-expanded-entries relative to dirname])"""
-    return (dirname, glob(os.path.join(dirname, entry)))
-
-
-if __name__ == '__main__':
-    main()
