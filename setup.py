ce1ae54cdd8974dfd8ac77eb0b485e09e3fedecd
wu <wu@.com>
2025-08-30 12:23:53 +0800

    run click-contrib/click-man/gpt-4o-1

--- setup.py
+++ setup.py
@@ -1,38 +1,38 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
-    README = readme.read()
 
-# allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 setup(
-    name='django-db-logger',
-    version='0.1.11',
-    packages=['django_db_logger', 'django_db_logger.migrations'],
-    include_package_data=True,
-    license='MIT License',
-    description='Django logging in database',
-    long_description=README,
-    url='https://github.com/CiCiUi/django-db-logger',
-    author='zhangshine',
-    author_email='zhangshine0125@gmail.com',
-    install_requires=['django>=1.9', 'six'],
+    name='click-man',
+    version='0.4.2',
+    url='https://github.com/click-contrib/click-man',
+    license='MIT',
+    description='Generate man pages for click based CLI applications',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    author='Timo Furrer',
+    author_email='tuxtimo@gmail.com',
+    install_requires=[
+        'click',
+        'setuptools',
+    ],
+    packages=find_packages(exclude=('tests', )),
+    entry_points={
+        'console_scripts': [
+            'click-man = click_man.__main__:cli',
+        ]
+    },
     classifiers=[
-        'Environment :: Web Environment',
-        'Framework :: Django',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Documentation',
     ],
 )
