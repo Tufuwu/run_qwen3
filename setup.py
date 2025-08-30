410c27cbe879f1b9715fb4af5052154b0c019f22
wu <wu@.com>
2025-08-30 12:21:07 +0800

    run bundlewrap/bundlewrap/gpt-4o-1

--- setup.py
+++ setup.py
@@ -1,49 +1,48 @@
-from setuptools import setup
-import ast
-import os
-import io
-
-
-def version():
-    """Return version string."""
-    with open(os.path.join("curtsies", "__init__.py")) as input_file:
-        for line in input_file:
-            if line.startswith("__version__"):
-                return ast.parse(line).body[0].value.s
-
-
-def long_description():
-    with open("README.md", encoding="utf-8") as f:
-        return f.read()
+from setuptools import find_packages, setup
 
 
 setup(
-    name="curtsies",
-    version=version(),
-    description="Curses-like terminal wrapper, with colored strings!",
-    long_description=long_description(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/bpython/curtsies",
-    author="Thomas Ballinger",
-    author_email="thomasballinger@gmail.com",
-    license="MIT",
-    packages=["curtsies"],
-    install_requires=[
-        "blessings>=1.5",
-        "wcwidth>=0.1.4",
-    ],
-    tests_require=[
-        "pyte",
-        "pytest",
-    ],
+    name="bundlewrap",
+    version="4.3.0",
+    description="Config management with Python",
+    long_description=(
+        "By allowing for easy and low-overhead config management, BundleWrap fills the gap between complex deployments using Chef or Puppet and old school system administration over SSH.\n"
+        "While most other config management systems rely on a client-server architecture, BundleWrap works off a repository cloned to your local machine. It then automates the process of SSHing into your servers and making sure everything is configured the way it's supposed to be. You won't have to install anything on managed servers."
+    ),
+    author="Torsten Rehn",
+    author_email="torsten@rehn.email",
+    license="GPLv3",
+    url="http://bundlewrap.org",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            "bw=bundlewrap.cmdline:main",
+        ],
+    },
+    keywords=["configuration", "config", "management"],
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: POSIX",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: System :: Installation/Setup",
+        "Topic :: System :: Systems Administration",
+    ],
+    install_requires=[
+        "cryptography",
+        "Jinja2",
+        "Mako",
+        "passlib",
+        "pyyaml",
+        "requests >= 1.0.0",
+        "tomlkit",
     ],
     zip_safe=False,
 )
