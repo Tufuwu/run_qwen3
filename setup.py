2d3deabd13682d318a461ae24d80174629f0b657
wu <wu@.com>
2025-08-30 12:24:31 +0800

    run coleifer/micawber/gpt-4o-1

--- setup.py
+++ setup.py
@@ -1,15 +1,36 @@
-# -*- coding: utf-8 -*-
-import sys
+import os
+from setuptools import setup, find_packages
 
-from pkg_resources import VersionConflict, require
-from setuptools import setup
+f = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
+readme = f.read()
+f.close()
 
-try:
-    require('setuptools>=38.3')
-except VersionConflict:
-    print("Error: version of setuptools is too old (<38.3)!")
-    sys.exit(1)
-
-
-if __name__ == "__main__":
-    setup()
+setup(
+    name='micawber',
+    version='0.5.1',
+    description='a small library for extracting rich content from urls',
+    long_description=readme,
+    author='Charles Leifer',
+    author_email='coleifer@gmail.com',
+    url='http://github.com/coleifer/micawber/',
+    packages=[p for p in find_packages() if not p.startswith('examples')],
+    package_data = {
+        'micawber': [
+            'contrib/mcdjango/templates/micawber/*.html',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Framework :: Django',
+    ],
+    test_suite='runtests.runtests',
+)
