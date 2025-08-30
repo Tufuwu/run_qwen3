e716c9a3c3e733924b361b452432e80fa1c428f6
wu <wu@.com>
2025-08-30 12:45:00 +0800

    run jllorencetti/pytest-deadfixtures/gpt-4o-1

--- setup.py
+++ setup.py
@@ -1,45 +1,75 @@
-#!/usr/bin/env python
-
+import codecs
 import os
-import sys
+import re
 
-from setuptools import setup
+from setuptools import Command, setup
 
-from lora import VERSION
+BASE_PATH = os.path.abspath(os.path.dirname(__file__))
 
-package_name = "python-lora"
 
-if sys.argv[-1] == "publish":
-    os.system("python setup.py sdist")
-    os.system("twine upload -r pypi dist/%s-%s.tar.gz" % (package_name, VERSION))
-    sys.exit()
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return codecs.open(file_path, encoding="utf-8").read()
 
-if sys.argv[-1] == "tag":
-    os.system("git tag -a v{} -m 'tagging v{}'".format(VERSION, VERSION))
-    os.system("git push && git push --tags")
-    sys.exit()
+
+def get_version():
+    changes_path = os.path.join(BASE_PATH, "CHANGES.rst")
+    regex = r"^#*\s*(?P<version>[0-9]+\.[0-9]+(\.[0-9]+)?)$"
+    with codecs.open(changes_path, encoding="utf-8") as changes_file:
+        for line in changes_file:
+            res = re.match(regex, line)
+            if res:
+                return res.group("version")
+    return "0.0.0"
+
+
+version = get_version()
+
+
+class VersionCommand(Command):
+    description = "print current library version"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        print(version)
 
 
 setup(
-    name="python-lora",
-    version=VERSION,
-    description="Decrypt LoRa payloads",
-    url="https://github.com/jieter/python-lora",
-    author="Jan Pieter Waagmeester",
-    author_email="jieter@jieter.nl",
+    name="pytest-deadfixtures",
+    version=version,
+    author="João Luiz Lorencetti",
+    author_email="me@dirtycoder.net",
+    maintainer="João Luiz Lorencetti",
+    maintainer_email="me@dirtycoder.net",
     license="MIT",
+    url="https://github.com/jllorencetti/pytest-deadfixtures",
+    description="A simple plugin to list unused fixtures in pytest",
+    long_description=read("README.rst"),
+    py_modules=["pytest_deadfixtures"],
+    install_requires=["pytest>=3.0.0"],
     classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Pytest",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Testing",
+        "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
     ],
-    keywords="LoRa decrypt",
-    packages=["lora"],
-    install_requires=["cryptography==3.2"],
+    cmdclass={"version": VersionCommand},
+    entry_points={"pytest11": ["deadfixtures = pytest_deadfixtures"]},
 )
