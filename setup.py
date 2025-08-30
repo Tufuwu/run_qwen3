1d738e23c1df19882136c405d8b27ee41c3c36e6
wu <wu@.com>
2025-08-30 13:26:42 +0800

    run wimglenn/pytest-structlog/gpt-4o-1

--- setup.py
+++ setup.py
@@ -1,62 +1,23 @@
-#!/usr/bin/env python3
+from setuptools import setup
 
-try:
-    from setuptools import setup, Command
-except ImportError:
-    from distutils.core import setup, Command
-
-
-def readme():
-    with open('README.rst') as f:
-        return f.read()
-
-
-def get_version(short=False):
-    with open('README.rst') as f:
-        for line in f:
-            if ':Version:' in line:
-                ver = line.split(':')[2].strip()
-                if short:
-                    subver = ver.split('.')
-                    return '%s.%s' % tuple(subver[:2])
-                else:
-                    return ver
-
-
-setup(name='fgivenx',
-      version=get_version(),
-      description='fgivenx: Functional Posterior Plotter',
-      long_description=readme(),
-      author='Will Handley',
-      author_email='wh260@cam.ac.uk',
-      url='https://github.com/williamjameshandley/fgivenx',
-      packages=['fgivenx', 'fgivenx.test'],
-      install_requires=['matplotlib', 'numpy', 'scipy'],
-      setup_requires=['pytest-runner'],
-      extras_require={
-          'docs': ['sphinx', 'sphinx_rtd_theme', 'numpydoc'],
-          'parallel': ['joblib'],
-          'progress_bar': ['tqdm'],
-          'getdist_chains': ['getdist']
-          },
-      tests_require=['pytest', 'pytest-mpl'],
-      include_package_data=True,
-      license='MIT',
-      classifiers=[
-                   'Development Status :: 5 - Production/Stable',
-                   'Intended Audience :: Developers',
-                   'Intended Audience :: Science/Research',
-                   'Natural Language :: English',
-                   'License :: OSI Approved :: MIT License',
-                   'Programming Language :: Python :: 2.7',
-                   'Programming Language :: Python :: 3.4',
-                   'Programming Language :: Python :: 3.5',
-                   'Programming Language :: Python :: 3.6',
-                   'Topic :: Scientific/Engineering',
-                   'Topic :: Scientific/Engineering :: Astronomy',
-                   'Topic :: Scientific/Engineering :: Physics',
-                   'Topic :: Scientific/Engineering :: Visualization',
-                   'Topic :: Scientific/Engineering :: Information Analysis',
-                   'Topic :: Scientific/Engineering :: Mathematics',
-      ],
-      )
+setup(
+    name="pytest-structlog",
+    version="0.5",
+    url="https://github.com/wimglenn/pytest-structlog",
+    description="Structured logging assertions",
+    long_description=open("README.rst").read(),
+    long_description_content_type="text/x-rst; charset=UTF-8",
+    author="Wim Glenn",
+    author_email="hey@wimglenn.com",
+    license="MIT",
+    install_requires=["pytest", "structlog"],
+    py_modules=["pytest_structlog"],
+    entry_points={"pytest11": ["pytest-structlog=pytest_structlog"]},
+    classifiers=[
+        "Framework :: Pytest",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+    options={"bdist_wheel": {"universal": "1"}},
+)
