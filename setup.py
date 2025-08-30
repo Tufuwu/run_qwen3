8c08a3dd58bea3487f656acdec0e9246561b18f2
wu <wu@.com>
2025-08-30 12:35:05 +0800

    run eskerda/pybikes/gpt-4o-1

--- setup.py
+++ setup.py
@@ -1,16 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2010-2012, eskerda <eskerda@gmail.com>
+# Distributed under the AGPL license, see LICENSE.txt
+
 from setuptools import setup
 
-
-setup(name='slotmachine',
-      version='0.0.5',
-      description='Conference talk scheduler',
-      author='EMF',
-      author_email='russ@emfcamp.org',
-      url='https://github.com/emfcamp/slotmachine',
-      packages=['slotmachine'],
-      install_requires=['PuLP==2.1',
-                        'python-dateutil>=2.5.3'],
-      license='MIT License',
-      zip_safe=False,
-      keywords='',
-      classifiers=[])
+setup(
+    name="pybikes",
+    version="1.0",
+    author="Lluis Esquerda",
+    author_email="eskerda@gmail.com",
+    packages=["pybikes"],
+    package_data={
+        'pybikes': ['data/*.json', 'kml/*.kml', 'kml/*.kml.gz'],
+    },
+    license="LICENSE.txt",
+    description="A python library for scrapping bike sharing data",
+    long_description=open('README.md').read(),
+    install_requires=[
+        'requests>=2.20.0',
+        'lxml',
+        'cssselect>=0.9',
+        'shapely>=1.5.13',
+    ],
+)
